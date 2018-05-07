16e055c044761338328d8f56238f6718ef0fb846
Eelco van Vliet <e.vanvliet@cbs.nl>
2018-05-07 14:05:57 +0200

    working on different formats

--- src/cbs_tools/kvk_url_extraction.py
+++ src/cbs_tools/kvk_url_extraction.py
@@ -17,6 +17,8 @@ __author__ = "Eelco van Vliet"
 __copyright__ = "Eelco van Vliet"
 __license__ = "mit"
 
+CACHE_TYPES = ["msg_pack", "hdf"]
+
 logging.basicConfig()
 _logger = logging.getLogger(__name__)
 
@@ -40,10 +42,18 @@ class KvKUrlParser(object):
         Reset the data base file in case this flag is True
     """
 
-    def __init__(self, input_file_name, reset_database=False, compress_format=None):
+    def __init__(self, input_file_name, reset_database=False, compress_format=None,
+                 cache_type=None):
 
         self.input_file_name = input_file_name
-        self.output_file_name = os.path.splitext(input_file_name)[0] + ".h5"
+        self.cache_type = cache_type
+        if cache_type == "msg_pack":
+            self.output_file_name = os.path.splitext(input_file_name)[0] + ".msg_pack"
+        elif cache_type == "hdf":
+            self.output_file_name = os.path.splitext(input_file_name)[0] + ".h5"
+        else:
+            raise AssertionError("Cache type can only one of the following: {}. Found {}"
+                                 "".format(CACHE_TYPES, cache_type))
         self.reset_database = reset_database
 
         self.compress_format = compress_format
@@ -62,30 +72,64 @@ class KvKUrlParser(object):
         """
 
         if self.reset_database or not os.path.exists(self.output_file_name):
+            # we are running the script for the first time or we want to reset the cache, so
+            # read the original csv data and store it to cache
             _logger.info("Reading data from original data base {name}"
                          "".format(name=self.input_file_name))
             self.data = pd.read_csv(self.input_file_name, header=None, usecols=[1, 2, 4],
                                     names=["KvK", "Name", "URL"])
 
+            # clean the data base a bit and set the kvk number as the index of the database
             self.data.fillna("", inplace=True)
             self.data.set_index(["KvK"], drop=True, inplace=True)
 
-            # explicitly convert the data to strings
+            # for hdf 5 we need to explicitly convert the data to strings
             for column_name in self.data.columns:
                 _logger.debug("Converting {}".format(column_name))
                 self.data[column_name] = self.data[column_name].astype(str)
 
-            _logger.info("Dumping data to cache file {} ".format(self.output_file_name))
-            self.data.to_hdf(self.output_file_name, key="kvk_data", mode="w", dropna=True,
-                             format="fixed")
+            # store the data to cache which we can read the next run
+            self.store_database_to_cache()
         else:
-            _logger.info("Reading data from cached file {name}"
-                         "".format(name=self.output_file_name))
-            self.data = pd.read_hdf(self.output_file_name)
+            # we have read the csv file before so now we can read from the cache
+            self.read_database_from_cache()
 
         _logger.info("Done reading file")
         _logger.debug("Data info \n{}".format(self.data.info))
         _logger.debug("Data head \n{}".format(self.data.head()))
+
+    def store_database_to_cache(self):
+        """
+        Store the data base to the cache file. Depending on the *cache_type* atttribute this can be
+        either message pack or hdf 5 format
+        """
+
+        _logger.info("Dumping data to cache file {} ".format(self.output_file_name))
+        if self.cache_type == "msg_pack":
+            # dump to message pack
+            self.data.to_msgpack(self.output_file_name, compress=self.compress_format)
+        elif self.cache_type == "hdf":
+            self.data.to_hdf(self.output_file_name, key="kvk_data", mode="w", dropna=True,
+                             format="fixed")
+        else:
+            raise AssertionError("Invalid cache type found: {} ".format(self.cache_type))
+
+    def read_database_from_cache(self):
+        """
+        Read the data base from the cache file
+        """
+
+        _logger.info("Reading data from cached file {name}".format(name=self.output_file_name))
+        if self.cache_type == "msg_pack":
+            self.data = pd.read_msgpack(self.output_file_name)
+        elif self.cache_type == "hdf":
+            self.data = pd.read_hdf(self.output_file_name)
+        else:
+            raise AssertionError("Invalid cache type found: {} ".format(self.cache_type))
+
+
+
+
 
 
 def _parse_the_command_line_arguments(args):
@@ -111,7 +155,9 @@ def _parse_the_command_line_arguments(args):
                         dest="log_level", const=logging.WARNING)
     parser.add_argument('--progressbar', help="Show a progress bar", action="store_true")
     parser.add_argument('--reset_database', help="Reset the data base in case we have generated"
-                                                 "a message pack file already", action="store_true")
+                                                 "a cache file already", action="store_true")
+    parser.add_argument('--cache_type', help="Type of the cache file ",
+                        choices=CACHE_TYPES, default="msg_pack")
 
     # parse the command line
     parsed_arguments = parser.parse_args(args)
@@ -136,6 +182,7 @@ def main(args_in):
     KvKUrlParser(
         input_file_name=args.input_file_name,
         reset_database=args.reset_database,
+        cache_type=args.cache_type,
     )
 
 
