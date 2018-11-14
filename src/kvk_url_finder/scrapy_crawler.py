b32dba14b505e14caa5b60e977812108bd854f9b
eelco <eelcovv@gmail.com>
2018-11-14 08:36:46 +0100

    working on new scraper

---
+++ src/kvk_url_finder/scrapy_crawler.py
@@ -0,0 +1,13 @@
+import scrapy
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+
+from kvk_url_finder.electronics import ElectronicsSpider
+
+configure_logging({"LOG_FORMAT": '%(levelname)s: %(message)'})
+runner = CrawlerRunner()
+
+d = runner.crawl(ElectronicsSpider)
+d.addBoth(lambda _: reactor.stop())
+reactor.run()
