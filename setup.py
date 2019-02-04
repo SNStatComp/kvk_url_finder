1e9c1e43f74419d0533345180b5b3db311552e45
EVLT <evlt@cbs.nl>
2019-02-04 12:33:56 +0100

    changed scriptname

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 # Add here console scripts and other entry points in ini-style format
 entry_points = """
 [console_scripts]
-kvk_url_finder = kvk_url_finder.find_kvk_urls:_run
+kvk_url_finder = kvk_url_finder.kvk_url_finder:_run
 # For example:
 # fibonacci = kvk_url_finder.skeleton:run
 """
