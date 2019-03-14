bc0220d33e0f79f8f2c014a975ef2c36f588a996
Eelco van Vliet <eelcovv@gmail.com>
2019-03-14 14:23:29 +0100

    addedd new class

--- src/kvk_url_finder/utils.py
+++ src/kvk_url_finder/utils.py
@@ -46,9 +46,7 @@ class UrlAnalyse(object):
             self.zip_codes = self.get_patterns(r"(\d{4}\s{0,1}[a-zA-Z]{2})")
             self.kvk_numbers = self.get_patterns(r"(\d{7,8})")
 
-            has_frames = self.soup.find('frame')
-            if has_frames:
-                self.dynamic_scrape()
+            self.has_frames = self.soup.find('frame')
 
     def dynamic_scrape(self):
 
@@ -108,3 +106,50 @@ class UrlAnalyse(object):
                 matches.append(grp)
 
         return matches
+
+
+class UrlDynAnalyse(object):
+
+    def __init__(self, url, store_page_to_cache=True, timeout=1.0):
+
+        self.logger = logging.getLogger(LOGGER_BASE_NAME)
+
+        self.store_page_to_cache = store_page_to_cache
+        if not url.startswith('http://') and not url.startswith('https://'):
+            self.url = 'http://{:s}/'.format(url)
+        else:
+            self.url = url
+
+        self.timeout = timeout
+
+        self.exists = False
+
+        self.zip_codes = None
+        self.kvk_numbers = None
+
+        self.dynamic_scrape()
+
+    def dynamic_scrape(self):
+
+        pattern = r"(\d{4}\s{0,1}[a-zA-Z]{2})"
+        pattern_comp = re.compile(pattern)
+        driver.get(self.url)
+        #frames = result.find_element_by_class_name('frame')
+        frames = driver.find_elements_by_css_selector('frame')
+        zips = list()
+        for frame in frames:
+            src = frame.get_attribute('src')
+            if src:
+                driver.get(src)
+                alldiv = driver.find_elements_by_css_selector("div")
+                for div in alldiv:
+                    text = div.text
+                    match = pattern_comp.search(str(text))
+                    if bool(match):
+                        grp = match.group(1)
+                        zips.append(grp)
+
+        self.logger.debug(f"selenium found {zips}")
+
+        self.zip_codes.extend(zips)
+
