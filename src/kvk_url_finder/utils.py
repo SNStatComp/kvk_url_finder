76a6aad73c44da5e3e1f3087861f087f3cdcf561
Eelco van Vliet <evlt@cbs.nl>
2019-04-22 16:19:47 +0200

    pastestring fix

--- src/kvk_url_finder/utils.py
+++ src/kvk_url_finder/utils.py
@@ -249,27 +249,31 @@ def paste_strings(string_list: list, separator=",", max_length=256, max_cnt=1000
     """ join the string from the list upto a maximum length """
 
     # note that we reverse the list, we can can peel off from the back
-    result = separator.join(string_list)
-    # matches the fist item (? is non-greedy)
-    length = len(result)
-    if length > max_length:
-        result = result[-1:0:-1]  # reverse string to be able to peel off from the back
-        match = re.compile(r"^.*?" + separator)
-        cnt = 0
-        while length > max_length:
-            result = match.sub("", result)
-            length = len(result)
-            cnt += 1
-            if cnt > max_cnt:
-                try:
-                    result = result[:max_length]
-                except IndexError:
-                    pass
-                logger.warning("Max count reached. Something wrong ?")
-                break
+    try:
+        result = separator.join(string_list)
+    except TypeError:
+        result = None
+    else:
+        # matches the fist item (? is non-greedy)
+        length = len(result)
+        if length > max_length:
+            result = result[-1:0:-1]  # reverse string to be able to peel off from the back
+            match = re.compile(r"^.*?" + separator)
+            cnt = 0
+            while length > max_length:
+                result = match.sub("", result)
+                length = len(result)
+                cnt += 1
+                if cnt > max_cnt:
+                    try:
+                        result = result[:max_length]
+                    except IndexError:
+                        pass
+                    logger.warning("Max count reached. Something wrong ?")
+                    break
 
-        # reverse back
-        result = result[-1:0:-1]
+            # reverse back
+            result = result[-1:0:-1]
 
     if result == "":
         result = None
